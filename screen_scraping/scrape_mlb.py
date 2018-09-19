b34dc5ebdaf4a16f3be5109c4f28b5b52c03367b
Ruben Pierich <rpierich@hotmail.com>
2018-09-19 03:33:44 -0400

    Got to work with csv unicode strings.

--- screen_scraping/scrape_mlb.py
+++ screen_scraping/scrape_mlb.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+from backports import csv
+import io
 
 def print_property(link, clss, name_of_property):
     if "active" in clss: return
@@ -7,6 +9,8 @@ def print_property(link, clss, name_of_property):
 
     if (len(name) > 0):
         print(name_of_property, ":  ", unicode(name.contents[0]))
+        listToWriteValuesToCSV.append(unicode(name.contents[0]))
+        # listToWriteValuesToCSV.append(unicode(","))
         # print("active not in clss class == "+clss)
     # else:
     #     print("active in class class == "+clss)
@@ -14,53 +18,70 @@ def print_property(link, clss, name_of_property):
     # str(integer).decode("utf-8")
     # agent_info = u' '.join((name_of_property,name.contents[0])).encode('utf-8').strip()
     # print(agent_info)
+listToWritopeneValuesToCSV = []
 
 with open('/home/rpierich/delme/python_data_science/screen_scraping/Sortable Player Stats | MLB.com.html') as infile:
     soup = BeautifulSoup(infile , 'html.parser')
 
-    links = soup.find_all('tr', tabindex=0)
-    for link in links:
-        name = link.find_all('td', class_="dg-name_display_last_init")
+    with io.open("eggs.csv", 'w', newline='', encoding='utf-8') as f:
+        # writer = csv.writer(f)
+        spamwriter = csv.writer(f, delimiter=unicode(","),
+                        quotechar=unicode('|'), quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['RK','Player','Team','Pos','G','AB','R','H','2B','3B','HR','RBI','BB','SO','SB','CS','AVG','OBP','SLG','OPS','IBB','HBP','SAC','SF','TB','XBH','GDP','GO','AO','GO_AO','NP','PA'])
 
-        print_property(link, "dg-rank", "Rank")
+        links = soup.find_all('tr', tabindex=0)
+        for link in links:
+            listToWriteValuesToCSV = []
 
-        aa = name[0].find_all('a')
-        print(aa[0].contents[0])
+            name = link.find_all('td', class_="dg-name_display_last_init")
 
-        print_property(link, "dg-rank", "RK")
-        print_property(link, "dg-name_display_last_init", "Player")
-        print_property(link, "dg-team_abbrev", "Team")
-        print_property(link, "dg-pos", "Pos")
-        print_property(link, "dg-g", "G")
-        print_property(link, "dg-ab", "AB")
-        print_property(link, "dg-r", "R")
-        print_property(link, "dg-h", "H")
-        print_property(link, "dg-d", "2B")
-        print_property(link, "dg-t", "3B")
-        print_property(link, "dg-hr", "HR")
-        print_property(link, "dg-rbi", "RBI")
-        print_property(link, "dg-bb", "BB")
-        print_property(link, "dg-so", "SO")
-        print_property(link, "dg-sb", "SB")
-        print_property(link, "dg-cs", "CS")
-        print_property(link, "dg-avg", "AVG")
-        print_property(link, "dg-obp", "OBP")
-        print_property(link, "dg-slg", "SLG")
-        print_property(link, "dg-ops", "OPS")
-        print_property(link, "dg-ibb", "IBB")
-        print_property(link, "dg-hbp", "HBP")
-        print_property(link, "dg-sac", "SAC")
-        print_property(link, "dg-sf", "SF")
-        print_property(link, "dg-tb", "TB")
-        print_property(link, "dg-xbh", "XBH")
-        print_property(link, "dg-gidp", "GDP")
-        print_property(link, "dg-go", "GO")
-        print_property(link, "dg-ao", "AO")
-        print_property(link, "dg-go_ao", "GO_AO")
-        print_property(link, "dg-np", "NP")
-        print_property(link, "dg-tpa", "PA")
+            print_property(link, "dg-rank", "Rank")
 
-        # name = link.find_all('td', class_="dg-rank")[0]
-        #
-        # print(name.contents[0])
-        print("\n\n")
+            aa = name[0].find_all('a')
+            print(aa[0].contents[0])
+
+            # with open('eggs.csv', 'wb') as csvfile:
+            #     spamwriter = csv.writer(csvfile, delimiter=' ',
+            #                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            #     spamwriter.writerow(['RK','Player','Team','Pos','G','AB','R','H','2B','3B','HR','RBI','BB','SO','SB','CS','AVG','OBP','SLG','OPS','IBB','HBP','SAC','SF','TB','XBH','GDP','GO','AO','GO_AO','NP','PA'])
+
+            print_property(link, "dg-rank", "RK")
+            print_property(link, "dg-name_display_last_init", "Player")
+            print_property(link, "dg-team_abbrev", "Team")
+            print_property(link, "dg-pos", "Pos")
+            print_property(link, "dg-g", "G")
+            print_property(link, "dg-ab", "AB")
+            print_property(link, "dg-r", "R")
+            print_property(link, "dg-h", "H")
+            print_property(link, "dg-d", "2B")
+            print_property(link, "dg-t", "3B")
+            print_property(link, "dg-hr", "HR")
+            print_property(link, "dg-rbi", "RBI")
+            print_property(link, "dg-bb", "BB")
+            print_property(link, "dg-so", "SO")
+            print_property(link, "dg-sb", "SB")
+            print_property(link, "dg-cs", "CS")
+            print_property(link, "dg-avg", "AVG")
+            print_property(link, "dg-obp", "OBP")
+            print_property(link, "dg-slg", "SLG")
+            print_property(link, "dg-ops", "OPS")
+            print_property(link, "dg-ibb", "IBB")
+            print_property(link, "dg-hbp", "HBP")
+            print_property(link, "dg-sac", "SAC")
+            print_property(link, "dg-sf", "SF")
+            print_property(link, "dg-tb", "TB")
+            print_property(link, "dg-xbh", "XBH")
+            print_property(link, "dg-gidp", "GDP")
+            print_property(link, "dg-go", "GO")
+            print_property(link, "dg-ao", "AO")
+            print_property(link, "dg-go_ao", "GO_AO")
+            print_property(link, "dg-np", "NP")
+            print_property(link, "dg-tpa", "PA")
+
+            # name = link.find_all('td', class_="dg-rank")[0]
+            #
+            # print(name.contents[0])
+            for ele in listToWriteValuesToCSV:
+                print(ele)
+            print("\n\n"+listToWriteValuesToCSV)
+            spamwriter.writerow(listToWriteValuesToCSV)
